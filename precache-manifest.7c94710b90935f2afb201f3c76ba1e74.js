7b80c4e783ccc720ef840feddb0c9bd29491048d
vkomolov <vkomolov1974@gmail.com>
2019-08-30 07:12:02 +0300

    to set gh-pages

---
+++ precache-manifest.7c94710b90935f2afb201f3c76ba1e74.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "4e0515a72e84845d8163",
+    "url": "./static/js/main.4e0515a7.chunk.js"
+  },
+  {
+    "revision": "457b2dc9dbb5b8d57f1b",
+    "url": "./static/js/1.457b2dc9.chunk.js"
+  },
+  {
+    "revision": "4e0515a72e84845d8163",
+    "url": "./static/css/main.253cc7f6.chunk.css"
+  },
+  {
+    "revision": "5ff3b410d838a8ebe0ae13d4f681fe1d",
+    "url": "./index.html"
+  }
+];
--- src/containers/Footer/Footer.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-
-///components
-import Footer from './';
-
-it("renders without crashing", () => {
-    shallow(<Footer/>);
-});
